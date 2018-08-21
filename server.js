333cc429ad7a7dbfbf7c5ec7db5b55ff201bbeda
deb <debkbanerji@gmail.com>
2018-08-21 10:50:25 -0400

    Create Initial Project
    Add node server, dependencies and Angular project

---
+++ server.js
@@ -0,0 +1,44 @@
+// Get dependencies
+const express = require('express');
+const path = require('path');
+const http = require('http');
+
+// Get our API routes
+const api = require(path.join(__dirname, 'api', 'api.js'));
+
+const app = express();
+
+/**
+ * Get port from environment and store in Express.
+ */
+const port = process.env.PORT || '3000';
+app.set('port', port);
+console.log("Node server running on port " + port);
+
+const frontendFolder = path.join(__dirname, 'static', 'templyte', 'dist', 'templyte');
+// Point static path to frontend folder
+app.use(express.static(frontendFolder));
+console.log("Serving static from " + frontendFolder);
+
+// Set our api routes
+app.use('/api', api);
+
+console.log("Setting api routes");
+
+// Catch all other routes and return the index file
+app.get('*', (req, res) => {
+    res.sendFile(path.join(frontendFolder, 'index.html'));
+});
+
+console.log("Catching all other routes and returning the index file");
+
+/**
+ * Create HTTP server.
+ */
+const server = http.createServer(app);
+
+console.log("Created Server");
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port, () => console.log(`Server running on port: ${port}`));
---
+++ static/templyte/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+
+import {AppComponent} from './app.component';
+import {RouterModule, Routes} from "@angular/router";
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+    {path: '', component: HomeComponent, pathMatch: 'full'},
+    {path: 'login', component: LoginComponent, pathMatch: 'full'},
+    {path: '**', redirectTo: '', pathMatch: 'full'} // Redirect everything else to the home page
+];
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        LoginComponent,
+        HomeComponent
+    ],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(routes)
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule {
+}
