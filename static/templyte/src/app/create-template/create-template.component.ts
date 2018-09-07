ac8e0ef3170a6838e5c7be6ca0b7c0f47d69e5f8
Theresa Mayo <theresamayo0@gmail.com>
2018-09-07 11:18:31 -0400

    Removed TODO for error handling

--- static/templyte/src/app/create-template/create-template.component.ts
+++ static/templyte/src/app/create-template/create-template.component.ts
@@ -87,29 +87,26 @@ export class CreateTemplateComponent implements OnInit {
             /*store data from the current typescript component in its own variable
                            because from within the upload function callbacks 'this' will refer to the current function being executed*/
             this.uploadFile(function (templateUrl) {
-            console.log(templateUrl);
-            const targetTemplateUrl = templateUrl;
-            const renderInfoObject = component.db.list('template-render-info');
-            renderInfoObject.push({
-                'template-url': targetTemplateUrl,
-                'variables' : component.variableArray,
-                'fileEndings' : component.fileEndingsArray
-            }).then((renderInfoResult) => {
-                const targetKey = renderInfoResult.key;
-                const directoryObject = component.db.object('template-directory/' + targetKey);
-                directoryObject.set({
-                    'templateName' : component.templateName,
-                    'tags' : component.tagArray,
-                    'authorName' : component.user.displayName,
-                    'authorUID' : component.user.uid,
-                    'authorPhotoUrl' : component.user.photoURL
-                })
-            }).then(() => {
-                this.dialog.open(UploadSuccessDialogComponent, {
-                width: '250px'
-            });}, (error) => { //TODO: Figure out why this won't work
-                console.log(error);
-                component.dialog.open(InputValidateDialogComponent);
+                console.log(templateUrl);
+                const targetTemplateUrl = templateUrl;
+                const renderInfoObject = component.db.list('template-render-info');
+                renderInfoObject.push({
+                    'template-url': targetTemplateUrl,
+                    'variables' : component.variableArray,
+                    'fileEndings' : component.fileEndingsArray
+                }).then((renderInfoResult) => {
+                    const targetKey = renderInfoResult.key;
+                    const directoryObject = component.db.object('template-directory_/' + targetKey);
+                    directoryObject.set({
+                        'templateName' : component.templateName,
+                        'tags' : component.tagArray,
+                        'authorName' : component.user.displayName,
+                        'authorUID' : component.user.uid,
+                        'authorPhotoUrl' : component.user.photoURL
+                    })
+                }).then(() => {
+                    component.dialog.open(UploadSuccessDialogComponent, {
+                    width: '250px'});
                 });
             });
             component.router.navigate(['home']);
