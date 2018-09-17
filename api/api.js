0c2d11751fb6be6b558be59215240f8bef88665c
NLD <ngailapdi@gmail.com>
2018-09-16 21:17:18 -0400

    communicate with backend

--- api/api.js
+++ api/api.js
@@ -9,5 +9,13 @@ router.get('/test-api', function (req, res) {
     res.send({message:'API works!'});
 });
 
+router.post('/download-template', (req, res) => {
+    data = req.body;
+    console.log("recevie data from client")
+    console.log(data);
+    // TODO: send rendered zip file back to client.
+    res.send({data}); 
+});
+
 console.log('Exporting api router');
 module.exports = router;
